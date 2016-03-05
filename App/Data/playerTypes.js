3021731e249d3bdde4cb7ae6a82331e5f78210ec
tiagojdferreira <tiagojdferreira@gmail.com>
2016-03-06 00:23:49 +0100

    Add type image. Needs fix
    Need to find better quality image. Need to try different layouts

--- App/Data/playerTypes.js
+++ App/Data/playerTypes.js
@@ -3,15 +3,18 @@ export const playerTypes = [
     name: 'attack',
     description: 'This type specializes in combat and using weapons.',
     skills: ['Toughness', 'Power', 'Weapon Focus'],
+    image: require('ryuutama/App/assets/type/attack.jpg')
   },
   {
     name: 'technical',
     description: 'This type can quickly assess a situation and  nd clever ways to solve problems.',
     skills: ['Accurate', 'Quick', 'Pocket'],
+    image: require('ryuutama/App/assets/type/technical.jpg')
   },
   {
     name: 'magic',
     description: 'This type enables the use of various types of magic.',
     skills: ['Will', 'Spellbook', 'Seasonal Sorcerer'],
+    image: require('ryuutama/App/assets/type/magic.jpg')
   },
 ]
