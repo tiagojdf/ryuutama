d8d2719e4b8e21916a76033f995b9f4e698a218f
tiagojdferreira <tiagojdferreira@gmail.com>
2016-03-08 22:30:35 +0100

    Add trads library

--- App/Components/Header/Header.js
+++ App/Components/Header/Header.js
@@ -3,16 +3,25 @@ import React, {
   View
 } from 'react-native'
 
+var I18n = require('react-native-i18n')
 import styles from './Header.styles'
 
 const Header = ({title}) => (
   <View style={styles.container}>
-    <Text style={styles.title}>{title}</Text>
+    <Text style={styles.title}>{I18n.t('title')}</Text>
   </View>
 )
 
 Header.propTypes = {
   title: React.PropTypes.string.isRequired,
+}
+
+I18n.fallbacks = true
+
+I18n.translations = {
+  en: {
+    title: 'Ryuutama'
+  }
 }
 
 module.exports = Header
