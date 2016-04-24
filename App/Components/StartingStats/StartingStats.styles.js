3bd11cefcc2243f5f60fce9acdfd63ccbdb55c8a
tiagojdferreira <tiagojdferreira@gmail.com>
2016-04-24 23:14:58 +0200

    Split StatSelector

--- App/Components/StartingStats/StartingStats.styles.js
+++ App/Components/StartingStats/StartingStats.styles.js
@@ -2,14 +2,31 @@ import {
   StyleSheet,
 } from 'react-native'
 
-// import styleVariables from '../../../Config/styleVariables'
+import styleVariables from '../../Config/styleVariables'
 
 export default StyleSheet.create({
   container: {
-    flex: 1
+    flex: 1,
+  },
+  statsContainer: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around'
   },
   invalid: {
     borderColor: 'red',
     borderWidth: 2,
-  }
+  },
+  select: {
+    backgroundColor: styleVariables.colors.springDarkGreen,
+    padding: 5,
+    borderRadius: 5,
+    margin: 20
+  },
+  selectText: {
+    textAlign:'center',
+    fontWeight: 'bold',
+    fontSize: 14,
+    color: 'white',
+  },
 })
