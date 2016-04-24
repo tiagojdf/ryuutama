f6ad19caed8693f2f152d6042a15be556855872f
tiagojdferreira <tiagojdferreira@gmail.com>
2016-04-24 18:08:35 +0200

    Update redux-persist

--- App/State/store.js
+++ App/State/store.js
@@ -1,11 +1,9 @@
 import { createStore, combineReducers } from 'redux'
-// import {AsyncStorage} from 'react-native'
+import {AsyncStorage} from 'react-native'
 import { player } from './player.reducer'
 import { step } from './step.reducer'
 
-// import {persistStore, autoRehydrate} from 'redux-persist'
-// const store = createStore(reducer, null, autoRehydrate())
-// persistStore(store)
+import {persistStore, autoRehydrate} from 'redux-persist'
 
 const store = (window.devToolsExtension ?
   window.devToolsExtension()(createStore)
@@ -18,8 +16,8 @@ const store = (window.devToolsExtension ?
     player: undefined,
     step: undefined,
   },
-  // autoRehydrate()
+  autoRehydrate()
 )
-// persistStore(store, {storage: AsyncStorage})
+persistStore(store, {storage: AsyncStorage})
 
 module.exports = store
